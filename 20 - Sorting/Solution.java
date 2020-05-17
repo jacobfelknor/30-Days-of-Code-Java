f156f35cb27288e63fdb091f129326d39d10f959
Jacob Felknor <jacobfelknor073@gmail.com>
2020-05-17 11:01:57 -0600

    sorting done

--- 20 - Sorting/Solution.java
+++ 20 - Sorting/Solution.java
@@ -1,3 +1,48 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
 public class Solution {
 
+    public static void swap(int a[], int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    public static int bubbleSort(int a[]) {
+        int totalSwaps = 0;
+        for (int i = 0; i < a.length; i++) {
+            int currentSwaps = 0;
+            for (int j = 0; j < a.length - 1; j++) {
+                if (a[j] > a[j + 1]) {
+                    swap(a, j, j + 1);
+                    currentSwaps++;
+                }
+            }
+            if (currentSwaps == 0) {
+                break;
+            } else {
+                totalSwaps += currentSwaps;
+            }
+        }
+
+        return totalSwaps;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] a = new int[n];
+        for (int a_i = 0; a_i < n; a_i++) {
+            a[a_i] = in.nextInt();
+        }
+        // Write Your Code Here
+        int swaps = bubbleSort(a);
+        System.out.println("Array is sorted in " + swaps + " swaps.");
+        System.out.println("First Element: " + a[0]);
+        System.out.println("Last Element: " + a[n - 1]);
+    }
 }
